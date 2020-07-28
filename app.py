3ccdf7652a6b1670170240d435bc0b215112c3b3
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-07-29 01:42:56 +0500

    fixed

--- app.py
+++ app.py
@@ -318,5 +318,5 @@ def create_app(test_config=None):
 APP = create_app()  
 
 if __name__ == '__main__':
-    # port = int(os.environ.get("PORT", 5000))
-    APP.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    APP.run(host='0.0.0.0', port=port, debug=True)
