15b58a03971da131db46791474d41d5f0cbd5bf6
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-07-28 16:51:55 +0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-from flask import request, _request_ctx_stack
+from flask import request, _request_ctx_stack, abort
 from jose import jwt
 import json
 from functools import wraps
@@ -111,6 +111,22 @@ def verify_decode_jwt(token):
                 'code': 'invalid_header',
                 'description': 'Unable to find the appropriate key.'
             }, 400)
+
+
+def check_permissions(permission, payload):
+    if 'permissions' not in payload:
+        raise AuthError({
+            'code': 'invalid_claims',
+            'description': 'Permissions not included in JWT.'
+        }, 400)
+
+    if permission not in payload['permissions']:
+        raise AuthError({
+            'code': 'unauthorized',
+            'description': 'Permission not found.'
+        }, 401)
+
+    return True            
             
 
 # decorater method
